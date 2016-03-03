0e5054cff2124cfae9f7be4711b5d834e959cc5f
simplecooler <shen.zeming@icloud.com>
2016-03-03 13:57:06 +0800

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <title></title>
+  </head>
+  <body>
+    <?php echo "hello world!"; ?>
+  </body>
+  <script src="script/jquery-2.2.1.min.js"></script>
+  <script src="script/script.js"></script>
+</html>

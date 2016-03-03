f7e066ebb8503211b884ada5df82d3db40249812
simplecooler <shen.zeming@icloud.com>
2016-03-03 14:27:55 +0800

    add image

--- manage.php
+++ manage.php
@@ -2,9 +2,12 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style/manage.css">
     <title></title>
   </head>
   <body>
-    <img src="image/logo.png" width="320" height="320" alt=""/>
+    <img class="logo" src="image/logo.png" alt="图片加载失败"/>
+    <div><input type="text" placeholder="用户名"/></div>
+    <div><input type="text" placeholder="密码"/></div>
   </body>
 </html>

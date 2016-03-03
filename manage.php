5b167c11da7b442902b9d771d93bb87f61579728
simplecooler <shen.zeming@icloud.com>
2016-03-03 17:29:12 +0800

    more

--- manage.php
+++ manage.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <img class="logo" src="image/logo.png" alt="图片加载失败"/>
-    <div><input type="text" placeholder="用户名"/></div>
-    <div><input type="text" placeholder="密码"/></div>
+    <div class="input"><input type="text" placeholder="用户名"/></div>
+    <div class="input"><input type="text" placeholder="密码"/></div>
   </body>
 </html>

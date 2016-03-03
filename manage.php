d646ce4fe19a7fd5e3da39d9c1fc508af414a433
simplecooler <shen.zeming@icloud.com>
2016-03-03 14:15:17 +0800

    create mansge

---
+++ manage.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <img src="image/logo.png" width="320" height="320" alt=""/>
+  </body>
+</html>

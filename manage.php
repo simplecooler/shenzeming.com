4e1955393ec1a92f9c27e6accbae75c6a9e288b9
simplecooler <shen.zeming@icloud.com>
2016-03-10 17:46:44 +0800

    complete check

--- manage.php
+++ manage.php
@@ -9,8 +9,22 @@
   </head>
   <body>
     <img class="logo" src="image/logo.png" alt="图片加载失败"/>
-    <div class="input"><input type="text" placeholder="用户名"/></div>
-    <div class="input"><input type="password" placeholder="密码"/></div>
-    <button class="submit-btn">登录</button>
+    <div class="input"><input id="user_name" type="text" placeholder="用户名"/></div>
+    <div class="input"><input id="password" type="password" placeholder="密码"/></div>
+    <button class="submit-btn" type="submit">登录</button>
+
+    <!-- 登录判断 -->
+    <script src="//cdn.bootcss.com/jquery/2.2.1/jquery.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $('.submit-btn').click(function(){
+          if(document.getElementById("user_name").value === 'shenzeming' && document.getElementById("password").value === '951753') {
+            window.location.href = "/manage_dashboard.php";
+          }else {
+            alert('非本组织成员，即将实施抓捕！');
+          }
+        });
+      });
+    </script>
   </body>
 </html>
---
+++ manage_dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style/manage_dashboard.css">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="bookmark" href="favicon.ico">
+    <title>神盾局秘密基地</title>
+  </head>
+  <body>
+    <div style="font-size: 30px;">欢迎局长进入基地！</div>
+  </body>
+</html>

03c76ee302a269b4b14836a9f2eae7aff0ba841b
simplecooler <shen.zeming@icloud.com>
2016-03-10 16:55:16 +0800

    change manage

--- manage.php
+++ manage.php
@@ -10,6 +10,7 @@
   <body>
     <img class="logo" src="image/logo.png" alt="图片加载失败"/>
     <div class="input"><input type="text" placeholder="用户名"/></div>
-    <div class="input"><input type="text" placeholder="密码"/></div>
+    <div class="input"><input type="password" placeholder="密码"/></div>
+    <button class="submit-btn">登录</button>
   </body>
 </html>

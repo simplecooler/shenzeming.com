09461aa53e749baff2ea8b42f56f782f8f66cf43
simplecooler <shen.zeming@icloud.com>
2016-05-18 16:59:46 +0800

    后端编写

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
 var fs = require('fs');
+var exec = require('child_process').exec;
 
-app.listen(8000);
+fs.appendFile('/etc/nginx/iplist.conf', 'deny 117.144.21.30;\n', (err) => {
+  if (err) throw err;
+  console.log('报告老大！可恶的攻击ip已经被列入黑名单了！');
+});
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.php',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+exec('nginx -s reload', function (error, stdout, stderr) {
+  console.log('报告老大！nginx已重新加载配置啦！');
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
 });
